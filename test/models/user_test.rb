c9c146721c910e83ff57f2233cc23b274de0f2ae
Benjamin Baka <baka@mpedigree.net>
2017-03-22 01:17:46 +0000

    Ben: Added tests to specify error message when name field is missing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save
   end
 
+  test "should ensure specified name error message" do
+    user = User.new
+    assert_not user.save
+    user.valid?
+    assert_equal user.errors.messages[:name], ["Name can not be empty. Please enter your name"]
+  end
+
   test "should not save user without email" do
     user = User.new
     user.name = "test1"
