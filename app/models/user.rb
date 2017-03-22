263d83881903a4b63cab451d02732e4f10a1d506
Benjamin Baka <baka@mpedigree.net>
2017-03-22 01:27:53 +0000

    Ben: Using new form of expressing dictionary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 
-  validates_presence_of :name, :message => "Name can not be empty. Please enter your name"
+  validates_presence_of :name, message: "Name can not be empty. Please enter your name"
   validates_presence_of :email
 end
