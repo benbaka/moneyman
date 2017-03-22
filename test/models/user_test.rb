9f01a2071a5b1da8686b3921d0e13d49716b2e0f
Benjamin Baka <baka@mpedigree.net>
2017-03-22 00:54:15 +0000

    Ben: Added test to make sure name and emails are required

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save user without a name" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save user without email" do
+    user = User.new
+    user.name = "test1"
+    assert_not user.save
+  end
+
 end
